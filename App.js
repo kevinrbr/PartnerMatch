26ec8bf903f81956db75a3bb10de2f8416220035
Kévin Robert <kevinrbr16@gmail.com>
2023-11-09 22:13:54 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Satoshi-Regular': require('./assets/fonts/Satoshi-Regular.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>PartnerMatch</Text>
+      <Text>aaaa</Text>
+      <Text style={styles.textStyle}>PartnerMatch</Text>
       <Text>Ouais ouais ouais</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,5 +36,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    fontFamily: 'Satoshi-Regular',
+    fontSize: 24,
   },
 });
