eb48414f84b0fc92a5cfb5af05ea46a1ebc8df76
Pasco <an.pascaud@gmail.com>
2023-11-20 22:36:28 +0100

    Refacto account.ts
    PAR-4

--- services/account.ts
+++ services/account.ts
@@ -1,28 +1,32 @@
-import { supabase } from "../supabase";
+import { supabaseAuth } from "./constants";
 
 export async function signInWithEmail(email: string, password: string) {
-    const { error, data } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      })
-    console.log(error);
-    return { data, error };
+  const { error, data } = await supabaseAuth.signInWithPassword({
+    email: email,
+    password: password,
+  })
+
+  console.log(error);
+
+  return { data, error };
 }
 
 export async function signUpWithPassword(email: string, password: string) {
-    const {
-        data: { session },
-        error,
-      } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-      });
-      return { error, session };
+  const {
+    data: { session },
+    error,
+  } = await supabaseAuth.signUp({
+    email: email,
+    password: password,
+  });
+
+  return { error, session };
 }
 
 export async function signOut() {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error('Erreur lors de la déconnexion:', error.message);
-    }
+  const { error } = await supabaseAuth.signOut();
+
+  if (error) {
+    console.error('Erreur lors de la déconnexion:', error.message);
+  }
 }
