92f0a97744c4790808dd1839a62dd3c30a289e70
Kévin Robert <kevinrbr16@gmail.com>
2023-12-07 09:39:52 +0100

    test lint

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,4 @@
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import Button from '@/components/Button'
