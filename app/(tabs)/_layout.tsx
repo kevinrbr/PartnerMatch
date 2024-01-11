436fa10ac90ebd0ef57e98ea3ceaae04ee820282
Kévin Robert <kevinrbr16@gmail.com>
2024-01-11 23:40:49 +0100

    add icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,42 @@
 import { Tabs } from 'expo-router'
+import {
+  HomeIcon,
+  ChatBubbleOvalLeftIcon,
+  UserCircleIcon,
+  PlusCircleIcon
+} from 'react-native-heroicons/outline'
 
 const StackLayout = () => {
   return (
     <Tabs initialRouteName="home">
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="messaging" />
-      <Tabs.Screen name="account" />
-      <Tabs.Screen name="researchForm" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: () => <HomeIcon color="#182A60" strokeWidth="1" />,
+          tabBarShowLabel: false
+        }}
+      />
+      <Tabs.Screen
+        name="messaging"
+        options={{
+          tabBarIcon: () => <ChatBubbleOvalLeftIcon color="#182A60" strokeWidth="1" />,
+          tabBarShowLabel: false
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          tabBarIcon: () => <UserCircleIcon color="#182A60" strokeWidth="1" />,
+          tabBarShowLabel: false
+        }}
+      />
+      <Tabs.Screen
+        name="researchForm"
+        options={{
+          tabBarIcon: () => <PlusCircleIcon color="#182A60" strokeWidth="1" />,
+          tabBarShowLabel: false
+        }}
+      />
     </Tabs>
   )
 }
